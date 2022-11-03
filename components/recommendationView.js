65ac5d2fe0190cd5e7bd4f384a423acce9fae8f9
KadynCBR <kadyn_martinez@hotmail.com>
2022-11-03 13:33:11 -0700

    combined push from second sprint.

--- components/recommendationView.js
+++ components/recommendationView.js
@@ -71,8 +71,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
+<<<<<<< HEAD
     backgroundColor: '#121212',
     position: "relative"
+=======
+    backgroundColor: '#121212'
+>>>>>>> 20ef42b (combined push from second sprint.)
   },
   item: {
     padding: 50,
