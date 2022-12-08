b8936abe244c96ae0be5e2960ec65719470b4eae
shapiroandre <shapiroandre@gmail.com>
2022-12-08 15:32:17 -0800

    3rd attempt

--- App.test.js
+++ App.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import RecommendationView from './components/recommendationView'
+// import RecommendationView from './components/recommendationView'
 import MainView from './components/mainView'
 import ColorPickerView from './components/ColorPickerView';
 import * as ColorHelpers from './utils/colorHelpers';
 
-import { render, screen, fireEvent } from '@testing-library/react-native';
-import { StyleSheet, Text, View, Button, Image, SafeAreaView, TouchableWithoutFeedback } from 'react-native';
+// import { render, screen, fireEvent } from '@testing-library/react-native';
+// import { StyleSheet, Text, View, Button, Image, SafeAreaView, TouchableWithoutFeedback } from 'react-native';
 describe('MainView', () => {
     it('renders correctly', () => {
         const tree = renderer.create(<MainView />).toJSON();
